83ead32cdac8043fc557dd28d781e0a8d903bafa
Talon Chandler <talonchandler@talonchandler.com>
2017-01-26 17:10:05 -0600

    Latex, fonts, preview latex, autocomplete

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -53,7 +53,7 @@
     ("~/GoogleDrive/capture.org" "~/GoogleDrive/reference.org" "~/GoogleDrive/projects.org")))
  '(package-selected-packages
    (quote
-    (mu4e-alert xterm-color cl-lib fill-column-indicator elpy auto-complete)))
+    (auctex mu4e-alert xterm-color cl-lib fill-column-indicator elpy auto-complete)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -63,6 +63,12 @@
  '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "light grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" : family "default"))))
  '(dired-flagged ((t (:inherit default))))
  '(dired-marked ((t (:inherit default) fault)))
+ '(font-latex-script-char-face ((t (:foreground "Red"))))
+ '(font-latex-sectioning-0-face ((t (:inherit font-latex-sectioning-1-face))))
+ '(font-latex-sectioning-1-face ((t (:inherit font-latex-sectioning-2-face))))
+ '(font-latex-sectioning-2-face ((t (:inherit font-latex-sectioning-3-face))))
+ '(font-latex-sectioning-3-face ((t (:inherit font-latex-sectioning-4-face))))
+ '(font-latex-sectioning-4-face ((t (:inherit font-latex-sectioning-5-face))))
  '(fringe ((t (:background "black"))))
  '(minibuffer-prompt ((t (:foreground "cyan"))))
  '(mode-line ((t (:background "grey80" :foreground "black" :box (:line-width 1 :color "grey80" :style released-button)))))
@@ -70,6 +76,7 @@
  '(mode-line-inactive ((t (:inherit mode-line :background "black" :foreground "grey80" :weight light))))
  '(mu4e-header-highlight-face ((t (:inherit region :weight bold))))
  '(mu4e-modeline-face ((t (:inherit mode-line-read-only-face :weight bold))))
+ '(preview-face ((t nil)))
  '(tool-bar ((t (:background "grey75" :foreground "black" :box (:line-width 1 :color "gray3" :style released-button)))))
  '(window-divider ((t (:foreground "black"))))
  '(window-divider-first-pixel ((t (:foreground "black"))))
