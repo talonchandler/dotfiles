b624ab7f2b97fe2ebeece2a70ceeae80adcf5ee2
Talon Chandler <talonchandler@talonchandler.com>
2017-12-02 14:24:50 -0500

    Diminish, beamer, unfill paragraph.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -51,7 +51,7 @@
  '(org-agenda-files (quote ("reference.org" "projects.org" "calendar/")))
  '(package-selected-packages
    (quote
-    (esup pdf-tools w3m forecast powerline twittering-mode magit emojify auctex mu4e-alert xterm-color cl-lib fill-column-indicator elpy auto-complete)))
+    (diminish esup pdf-tools w3m forecast powerline twittering-mode magit emojify auctex mu4e-alert xterm-color cl-lib fill-column-indicator elpy)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
