93c807a6dfe14c6c5d4b0d372e7d12d17004e3f8
Talon Chandler <talonchandler@talonchandler.com>
2022-03-17 14:01:44 -0700

    Misc. emacs updates.

--- .emacs.d/talon.el
+++ .emacs.d/talon.el
@@ -1,4 +1,4 @@
-
+(setq gc-cons-threshold 10000000)
 (package-initialize)
 (require 'package)
 (add-to-list 'package-archives
@@ -13,173 +13,300 @@
 (setq calendar-location-name "Chicago, IL")
 
 ;; Set paths
-(require 'mu4e)
-(setq mu4e-maildir "~/Mail")
-(setq mu4e-sent-folder "/[Gmail].Sent Mail")
+;; (require 'mu4e)
+;; (setq mu4e-maildir "~/Mail")
+;; (setq mu4e-sent-folder "/[Gmail].Sent Mail")
 
-;; Don't save messages to Sent Messages, Gmail/IMAP takes care of this
-;;(setq mu4e-sent-messages-behavior 'delete)
+;; ;; Don't save messages to Sent Messages, Gmail/IMAP takes care of this
+;; ;;(setq mu4e-sent-messages-behavior 'delete)
 
-;; Updating with crontab but just in case
-(setq mu4e-get-mail-command "true")
+;; ;; Updating with crontab but just in case
+;; (setq mu4e-get-mail-command "true")
 
-;; Auto-retrieve
-(setq mu4e-update-interval 15)
+;; ;; Auto-retrieve
+;; (setq mu4e-update-interval 30)
 
-(setq mu4e-index-cleanup nil)
-(setq mu4e-index-lazy-check t)
-(setq mu4e-hide-index-messages t)
+;; (setq mu4e-index-cleanup nil)
+;; (setq mu4e-index-lazy-check t)
+;; (setq mu4e-hide-index-messages t)
 
-;; Patch for changing UIDs
-(setq mu4e-change-filenames-when-moving t)
+;; ;; Patch for changing UIDs
+;; (setq mu4e-change-filenames-when-moving t)
 
-;; Shortcuts
-(setq mu4e-maildir-shortcuts
-    '( ("/INBOX"               . ?i)
-       ("/[Gmail].Sent Mail"   . ?s)
-       ("/[Gmail].Trash"       . ?t)
-       ("/[Gmail].All Mail"    . ?a)))
+;; ;; Shortcuts
+;; (setq mu4e-maildir-shortcuts
+;;     '( ("/INBOX"               . ?i)
+;;        ("/[Gmail].Sent Mail"   . ?s)
+;;        ("/[Gmail].Trash"       . ?t)
+;;        ("/[Gmail].All Mail"    . ?a)))
 
-;; Refile to
-(setq mu4e-refile-folder "/[Gmail].All Mail")
-(setq mu4e-headers-results-limit '500)
+;; ;; Refile to
+;; (setq mu4e-refile-folder "/[Gmail].All Mail")
+;; (setq mu4e-headers-results-limit '100)
 
-;; View html 
-(setq mu4e-html2text-command "w3m -T text/html")
-;;(setq mu4e-html2text-command 'mu4e-shr2text)
+;; ;; View html
+;; (setq mu4e-html2text-command "w3m -T text/html")
+;; ;;(setq mu4e-html2text-command 'mu4e-shr2text)
 
-(require 'smtpmail)
-(setq message-send-mail-function 'smtpmail-send-it
-   starttls-use-gnutls t
-   smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-   smtpmail-auth-credentials
-     '(("smtp.gmail.com" 587 "talonchandler@gmail.com" nil))
-   smtpmail-default-smtp-server "smtp.gmail.com"
-   smtpmail-smtp-server "smtp.gmail.com"
-   smtpmail-smtp-service 587)
+;; (require 'smtpmail)
+;; (setq message-send-mail-function 'smtpmail-send-it
+;;    starttls-use-gnutls t
+;;    smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+;;    smtpmail-auth-credentials
+;;      '(("smtp.gmail.com" 587 "talonchandler@gmail.com" nil))
+;;    smtpmail-default-smtp-server "smtp.gmail.com"
+;;    smtpmail-smtp-server "smtp.gmail.com"
+;;    smtpmail-smtp-service 587)
 
-(mu4e-alert-enable-notifications)
-(mu4e-alert-disable-notifications)
-(load-file "~/.emacs.d/vip.el")
-(add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
-(setq mu4e-alert-email-notification-types '(count))
+;; (mu4e-alert-enable-notifications)
+;; (mu4e-alert-disable-notifications)
+;; (load-file "~/.emacs.d/vip.el")
+;; (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
+;; (setq mu4e-alert-email-notification-types '(count))
 
-(defun my-mu4e-alert-mode-line-formatter (mail-count)
-  (when (not (zerop mail-count))
-    (concat " "
-            (if (zerop mail-count)
-                " "
-              (format "[%d UNREAD]" mail-count)))))
+;; (defun my-mu4e-alert-mode-line-formatter (mail-count)
+;;   (when (not (zerop mail-count))
+;;     (concat " "
+;;             (if (zerop mail-count)
+;;                 " "
+;;               (format "[%d UNREAD]" mail-count)))))
 
-(setq mu4e-alert-modeline-formatter 'my-mu4e-alert-mode-line-formatter)
+;; (setq mu4e-alert-modeline-formatter 'my-mu4e-alert-mode-line-formatter)
 
-;;(mu4e-alert-set-default-style 'growl)
-;;(add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-;;(alert-add-rule :category "mu4e-alert" :style 'fringe :predicate (lambda (_) (string-match-p "^mu4e-" (symbol-name major-mode))) :continue t)
-(mu4e-alert-enable-notifications)
+;; ;;(mu4e-alert-set-default-style 'growl)
+;; ;;(add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+;; ;;(alert-add-rule :category "mu4e-alert" :style 'fringe :predicate (lambda (_) (string-match-p "^mu4e-" (symbol-name major-mode))) :continue t)
+;; (mu4e-alert-enable-notifications)
 
 ;; Signature
-(setq
-   user-mail-address "talonchandler@gmail.com"
-   user-full-name  "Talon Chandler"
-   mu4e-compose-signature
-    (concat
-      "Talon Chandler\n"
-      "(312) 978-1901"))
+;; (setq
+;;    user-mail-address "talonchandler@gmail.com"
+;;    user-full-name  "Talon Chandler"
+;;    mu4e-compose-signature
+;;     (concat
+;;       "Talon Chandler\n"
+;;       "(312) 978-1901"))
 
-;; Which email addresses are mine
-(setq mu4e-user-mail-address-list
-'("talonchandler@gmail.com"
-  "talonchandler@talonchandler.com"
-  "talonchandler@uchicago.edu"))
+;; ;; Which email addresses are mine
+;; (setq mu4e-user-mail-address-list
+;; '("talonchandler@gmail.com"
+;;   "talonchandler@talonchandler.com"
+;;   "talonchandler@uchicago.edu"))
 
-;; Choose headers
-(setq mu4e-headers-fields
-'((:human-date . 10)
- (:flags . 4)
- (:from-or-to . 24)
- (:subject . 34)
- (:mailing-list . 10)))
+;; ;; Choose headers
+;; (setq mu4e-headers-fields
+;; '((:human-date . 10)
+;;  (:flags . 4)
+;;  (:from-or-to . 24)
+;;  (:subject . 34)
+;;  (:mailing-list . 10)))
 
-;; Execute all marks without confirmation.
-(defun my-mu4e-mark-execute-all-no-confirm ()
-   (interactive)
-   (mu4e-mark-execute-all 'no-confirm))
-(define-key mu4e-headers-mode-map "x" #'my-mu4e-mark-execute-all-no-confirm)
+;; ;; Execute all marks without confirmation.
+;; (defun my-mu4e-mark-execute-all-no-confirm ()
+;;    (interactive)
+;;    (mu4e-mark-execute-all 'no-confirm))
+;; (define-key mu4e-headers-mode-map "x" #'my-mu4e-mark-execute-all-no-confirm)
 
-;; Display
-(setq shr-color-visible-luminance-min 100)
+;; ;; Display
+;; (setq shr-color-visible-luminance-min 100)
 
-;; Turn on spell check and fill column
-(add-hook 'mu4e-compose-mode-hook
-  (defun my-do-compose-stuff ()
-    (set-fill-column 72)
-    (flyspell-mode)))
+;; ;; Turn on spell check and fill column
+;; (add-hook 'mu4e-compose-mode-hook
+;;   (defun my-do-compose-stuff ()
+;;     (set-fill-column 72)
+;;     (flyspell-mode)))
 
-;; Ask to quit
-(setq mu4e-confirm-quit t)
+;; ;; Ask to quit
+;; (setq mu4e-confirm-quit t)
 
-(global-set-key (kbd "C-c m") 'mu4e)
+;; ;; Turn off threads by default
+;; (setq mu4e-headers-show-threads nil)
+
+;; ;; Set number of visible header lines
+;; (setq mu4e-headers-visible-lines 14)
+
+;; ;; Truncate lines by default
+;; (setq truncate-lines t)
 
 (setq abbrev-file-name "~/.emacs.d/abbrev_defs")
 (setq save-abbrevs t)
 
 (setq bibtex-maintain-sorted-entries 't)
+(setq bibtex-align-at-equal-sign 't)
+(setq bibtex-autokey-year-length 4)
+(setq bibtex-autokey-titlewords 0)
+(setq bibtex-autokey-titleword-length 0)
+(setq bibtex-completion-pdf-extension ".pdf")
+(setq bibtex-completion-find-additional-pdfs nil)
+
+;; Cleaning from Nick Higham
+(setq bibtex-entry-format
+      '(page-dashes required-fields numerical-fields whitespace
+        last-comma delimiters unify-case sort-fields realign strings braces))
+
+(setq bibtex-field-delimiters 'double-quotes)
+(setq bibtex-entry-delimiters 'braces)
+(setq bibtex-field-braces-alist '((("title" "journal") "DNA")
+                                  (("title") "2D")
+                                  (("title") "3D")
+                                  (("title") "OTF")))
+
+;; I prefer closing brace on its own line after cleaning BibTeX entry.
+(setq bibtex-clean-entry-hook 'mybibtex-clean-extra)
+(defun mybibtex-clean-extra ()
+  (progn (bibtex-end-of-entry) (newline) (newline)))
+;; (bibtex-beginning-of-entry) (bibtex-next-field) ;; not working
+;;(newline) (delete-backward-character) ;; not working
+
+(setq bibtex-completion-additional-search-fields '(keywords))
+
+;; Faster navigation
+;; http://kitchingroup.cheme.cmu.edu/blog/2014/10/13/Navigating-your-bibtex-file/
+(defun bibtex-next-entry (&optional n)
+  "Jump to the beginning of the next bibtex entry. N is a prefix
+argument. If it is numeric, jump that many entries
+forward. Negative numbers do nothing."
+  (interactive "P")
+  ;; Note if we start at the beginning of an entry, nothing
+  ;; happens. We need to move forward a char, and call again.
+  (when (= (point) (save-excursion
+                     (bibtex-beginning-of-entry)))
+    (forward-char)
+    (bibtex-next-entry))
+
+  ;; search forward for an entry 
+  (when 
+      (re-search-forward bibtex-entry-head nil t (and (numberp n) n))
+    ;; go to beginning of the entry
+    (bibtex-beginning-of-entry)))
+
+(defun bibtex-previous-entry (&optional n)
+  "Jump to beginning of the previous bibtex entry. N is a prefix
+argument. If it is numeric, jump that many entries back."
+  (interactive "P")
+  (bibtex-beginning-of-entry)
+ (when 
+     (re-search-backward bibtex-entry-head nil t (and (numberp n) n))
+   (bibtex-beginning-of-entry)))
+
+(defun jmax-bibtex-mode-keys ()
+  "Modify keymaps used by `bibtex-mode'."
+  (local-set-key (kbd "M-n") 'bibtex-next-entry)
+  (local-set-key (kbd "M-p") 'bibtex-previous-entry))
+
+;; add to bibtex-mode-hook
+(add-hook 'bibtex-mode-hook 'jmax-bibtex-mode-keys)
+
+;; Open shortcut
+(global-set-key (kbd "C-x p") 'helm-bibtex)
+
+;; Locations
+(setq helm-bibtex-full-frame nil)
+(setq bibtex-completion-bibliography '("~/Library/texmf/talon.bib"))
+(setq bibtex-completion-library-path "~/GoogleDrive/reference/papers/")
+(setq bibtex-completion-notes-path "~/GoogleDrive/reference/papers/notes.org")
+
+;; Display setting
+(setq bibtex-completion-display-formats '((t . "${author:24} ${title:*} ${year:4} ${=type=:7} ${=has-pdf=:1}${=has-note=:1}")))
+(setq bibtex-completion-pdf-symbol "P")
+(setq bibtex-completion-notes-symbol "N")
+
+;; Open in preview
+(setq bibtex-completion-pdf-open-function
+  (lambda (fpath)
+    (call-process "open" nil 0 nil "-a" "/System/Applications/Preview.app" fpath)))
+
+;; Reverse Sorting
+(advice-add 'bibtex-completion-candidates
+            :filter-return 'reverse)
+
+;; Better formatting for bib insert reference
+(defun bibtex-completion-insert-reference (keys)
+  "Insert a reference for each selected entry."
+  (let* ((refs (--map
+                (s-word-wrap fill-column
+                             (concat "\n" (bibtex-completion-apa-format-reference it)))
+                keys)))
+    (insert "\n" (s-join "\n" refs) "\n")))
 
 (setq org-directory "/Users/Talon/GoogleDrive/")
+  (setq org-agenda-files '("reference.org" "projects.org" "calendar/"))
+  (setq org-hide-block-startup t)
 
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "CANCELED(c)" "DONE(d)")))
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "CANCELED(c)" "DONE(d)")))
 
-(setq org-todo-keyword-faces
-           '(("TODO" . "pink")
-             ("STARTED" . "yellow")
-             ("WAITING" . "orange")
-             ("CANCELED" . "red")
-             ("DONE" . "green")))
+  (setq org-todo-keyword-faces
+             '(("TODO" . "pink")
+               ("STARTED" . "yellow")
+               ("WAITING" . "orange")
+               ("CANCELED" . "red")
+               ("DONE" . "green")))
 
-(setq org-tag-alist '((:startgroup . nil)
-                     ("@work" . ?w) ("@home" . ?h) ("errand" . ?e)
-                     (:endgroup .nil)))
-(setq org-tags-column -85)
+  (setq org-tag-alist '((:startgroup . nil)
+                       ("@work" . ?w) ("@home" . ?h) ("errand" . ?e)
+                       (:endgroup .nil)))
+  (setq org-tags-column -85)
 
-(setq org-log-done 'time)
+  (setq org-log-done 'time)
 
-(setq org-default-notes-file "capture.org")
-(setq org-agenda-files (quote ("capture.org" "reference.org" "projects.org" "calendar/")))
-(setq org-archive-location "archive/datetree.org::datetree/* Finished Tasks")
-(setq org-enforce-todo-dependencies t)
-(setq org-agenda-include-diary t) ;; Read sexp diary entries
-(setq org-agenda-window-setup "current-window")
-(setq org-deadline-warning-days 7)
+  (setq org-default-notes-file "capture.org")
+  (setq org-agenda-files (quote ("reference.org" "projects.org" "calendar/")))
+  (setq org-archive-location "archive/datetree.org::datetree/* Finished Tasks")
+  (setq org-enforce-todo-dependencies t)
+  (setq org-agenda-include-diary t) ;; Read sexp diary entries
+  (setq org-agenda-window-setup "current-window")
+  (setq org-deadline-warning-days 7)
 
-(global-set-key (kbd "C-c a") 'org-agenda-list)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c l") 'org-store-link)
+  (global-set-key (kbd "C-c a") 'org-agenda-list)
+  (global-set-key (kbd "C-c c") 'org-capture)
+  (global-set-key (kbd "C-c l") 'org-store-link)
 
-;; Save window views
-(setq org-agenda-restore-windows-after-quit t)
+  (global-set-key (kbd "C-c s") (lambda () (interactive) (org-content 2)))
 
-;; Org quick done and archive
-(defun org-toggle-todo-and-fold ()
+  ;; Save window views
+  (setq org-agenda-restore-windows-after-quit t)
+
+  ;; Org quick done and archive
+  (defun org-toggle-todo-and-fold ()
+    (interactive)
+    (save-excursion
+      (org-back-to-heading t) ;; Make sure command works even if point is
+                              ;; below target heading
+      (cond ((looking-at "\*+ TODO")
+             (org-todo "DONE")
+             (hide-subtree))
+            ((looking-at "\*+ DONE")
+             (org-todo "TODO")
+             (hide-subtree))
+            (t (message "Can only toggle between TODO and DONE.")))))
+
+  (global-set-key (kbd "C-x C-d") 'org-toggle-todo-and-fold)
+
+  ;; Org export
+  (setq org-export-dispatch-use-expert-ui 't)
+
+  ;;(require 'org-mu4e)
+
+  ;; Start everything folded
+  (setq org-hide-block-startup 't)
+  (setq org-startup-folded 't)
+
+  ;;
+  (require 'calfw)
+  (require 'calfw-org)
+
+(defun my-open-calendar ()
   (interactive)
-  (save-excursion
-    (org-back-to-heading t) ;; Make sure command works even if point is
-                            ;; below target heading
-    (cond ((looking-at "\*+ TODO")
-           (org-todo "DONE")
-           (hide-subtree))
-          ((looking-at "\*+ DONE")
-           (org-todo "TODO")
-           (hide-subtree))
-          (t (message "Can only toggle between TODO and DONE.")))))
+  (cfw:open-calendar-buffer
+   :view 'two-weeks
+   :contents-sources
+   (list
+    (cfw:org-create-source "LightSkyBlue")  ; orgmode source
+   )))
+(global-set-key (kbd "C-x a") 'my-open-calendar)
 
-(global-set-key (kbd "C-x C-d") 'org-toggle-todo-and-fold)
-
-;; Org export
-(setq org-export-dispatch-use-expert-ui 't)
-
-(require 'org-mu4e)
+(setq cfw:render-line-breaker 'cfw:render-line-breaker-wordwrap)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
@@ -269,24 +396,24 @@
 ;; Enable math mode (type ` then letter for character)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
-;; Basic highlighting from here: http://chasen.org/~daiti-m/dist/math++.el
-;; Fancier stuff here if needed: https://github.com/kawabata/wolfram-mode
-(load-file "~/.emacs.d/math++.el")
-(add-to-list 'auto-mode-alist '("\\.wl\\'" . math-mode))
-
 (add-hook 'python-mode-hook 'fci-mode)
 (add-hook 'python-mode-hook 'linum-mode)
 (add-hook 'python-mode-hook 'abbrev-mode)
+
 ;;(add-hook 'LaTeX-mode-hook 'fci-mode)
 ;;(add-hook 'LaTeX-mode-hook 'linum-mode)
+(add-hook 'text-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (setq reftex-plug-into-AUCTeX t)
+
 (add-hook 'lisp-mode-hook 'linum-mode)
 (add-hook 'org-mode-hook 'org-indent-mode)
 (add-hook 'org-mode-hook (lambda () (diminish 'org-indent-mode "")))
 (add-hook 'org-mode-hook 'org-beamer-mode)
 (add-hook 'org-mode-hook (lambda () (diminish 'org-beamer-mode "")))
+
 (add-hook 'math-mode 'linum-mode)
 
 ;; Use ido
@@ -295,7 +422,8 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (setq ido-use-filename-at-point 'guess)
-(setq ido-file-extensions-order '(".org" ".txt" ".py"))
+(setq ido-file-extensions-order '(".tex" ".org" ".txt" ".py"))
+(setq ido-ignore-files '("\.ldf" "\.fdb_latexmk"))
 (setq ido-max-window-height 1)
 
 ;; Use autocomplete
@@ -311,6 +439,9 @@
 
 ;; GPG workaround: https://colinxy.github.io/software-installation/2016/09/24/emacs25-easypg-issue.html
 (setq epa-pinentry-mode 'loopback)
+
+;;
+(show-paren-mode)
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq backup-by-copying t)
@@ -351,6 +482,21 @@
 
 (setq frame-resize-pixelwise 'true)
 (setq line-spacing 0)
+
+(setq-default show-trailing-whitespace t)
+
+(dolist (hook '(special-mode-hook
+                term-mode-hook
+                comint-mode-hook
+                compilation-mode-hook
+                minibuffer-setup-hook
+                shell-mode-hook
+                calendar-mode-hook
+                org-agenda-mode-hook
+                buffer-menu-mode-hook
+                helm-mode-hook))
+                (add-hook hook
+                          (lambda () (setq show-trailing-whitespace nil))))
 
 (add-to-list 'display-buffer-alist
              '("*Help*" display-buffer-same-window))
@@ -398,6 +544,8 @@
          (select-window (funcall selector)))
        (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
  (global-set-key (kbd "C-x 7") 'transpose-windows)
+
+(global-set-key (kbd "C-x 4") (lambda () (interactive) (enlarge-window 8)))
 
 (global-set-key (kbd "C-x l") 'previous-buffer)
 (global-set-key (kbd "C-x ;") 'next-buffer)
@@ -497,9 +645,9 @@ i.e. change right window to bottom, or change bottom window to right."
 
 (global-set-key (kbd "C-c C-f") 'toggle-frame-fullscreen)
 
-;;; Opposite of fill-paragraph. https://www.emacswiki.org/emacs/UnfillParagraph
-    (defun unfill-paragraph (&optional region)
-      "Takes a multi-line paragraph and makes it into a single line of text."
+;; Opposite of fill-paragraph. https://www.emacswiki.org/emacs/UnfillParagraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
       (interactive (progn (barf-if-buffer-read-only) '(t)))
       (let ((fill-column (point-max))
             ;; This would override `fill-column' if it's an integer.
@@ -510,21 +658,20 @@ i.e. change right window to bottom, or change bottom window to right."
 
 (setq sentence-end-double-space nil)
 
-;; Place custom commands in another file
+;; Place custom commands in another file (typically appearance only)
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
 ;; Initial window layout
 ;; (when window-system (set-frame-size (selected-frame) 180 48))
 (find-file "~/.emacs.d/talon.org")
-(find-file "~/GoogleDrive/projects.org")
+(find-file "/Users/Talon/GoogleDrive/projects.org")
 (shell "*shell1*")
 (switch-to-buffer "projects.org")
 (org-agenda-list)
-(balance-windows)
+;; (my-open-calendar)
 (other-window 1)
+(window-toggle-split-direction)
 (kill-buffer "canada.org")
 (kill-buffer "america.org")
-(kill-buffer "journalclub.org")
 (kill-buffer "diary")
-(window-toggle-split-direction)
